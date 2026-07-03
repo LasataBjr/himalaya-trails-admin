f5ffa51b41ecd073b7a4aca3b5a1c8f5271a2f96
Lasata Bajracharya <lasatabjrktm@gmail.com>
2026-07-03 18:05:46 +0545

    fix: changed the style, added tailwindcss and mainlayout file

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,3 +1,32 @@
-
+import React from "react";
+import Sidebar from "../components/common/Sidebar";
+import Navbar from "../components/common/Navbar";
+import Footer from "../components/common/Footer";
 
 import { Outlet } from "react-router-dom";
+
+export default function MainLayout() {
+    return (
+        <div className="flex h-screen w-screen overflow-hidden bg-surface-bg antialiased">
+            {/* Left Navigation Block */}
+            <Sidebar />
+
+            {/* Right Workplace Content Canvas */}
+            <div className="flex flex-1 flex-col overflow-hidden">
+                {/* Top Control Header */}
+                <Navbar />
+
+                {/* Central Display Surface for Sub-Pages */}
+                <main className="flex-1 overflow-y-auto p-8">
+                <div className="max-w-7xl mx-auto w-full h-full">
+                    <Outlet />
+                </div>
+                </main>
+
+                {/* Bottom Platform Metric Footer */}
+                <Footer />
+            </div>
+        </div>
+    );
+
+}
